4d54f872dc9d6ba363dec0d96ea8545f60387bab
Kitsunebackfire <kwolz92@gmail.com>
2022-12-01 17:31:58 -0600

    fix method and action error on post forms as they were reversed, add error message for passwords not matching

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,21 @@
 const mongoose = require("mongoose");
-const userSchema = require("../models/userSchema.js");
+const User = require("../models/userSchema.js");
 
-exports.getSignup = (req, res) => {
-  res.render("signup", { title: "Sign Up" });
+exports.login = (req, res) => {
+  // home page for app. will designate directions to login, sign up, or guest view via links
+  res.render("index", { title: "Home Page" });
+};
+
+exports.signup__get = (req, res) => {
+  res.render("signup", { title: "Sign Up", passwordError: false });
+};
+
+exports.signup__post = async (req, res) => {
+  console.log(req.body);
+  if (req.body.password !== req.body.confirmPassword) {
+    console.log("passwords do not match");
+    res.render("signup", { title: "Sign Up", passwordError: true });
+  } else {
+    res.render("signup");
+  }
 };
--- routes/index.route.js
+++ routes/index.route.js
@@ -4,12 +4,11 @@ const userController = require("../controllers/user.controller.js");
 // imported user model
 const User = require("../models/userSchema");
 
-router.get("/", (req, res) => {
-  // home page for app. will designate directions to login, sign up, or guest view via links
-  res.render("index", { title: "Home Page" });
-});
+router.get("/", userController.login);
 
-router.get("/signUp", userController.getSignup);
+router.get("/signUp", userController.signup__get);
+
+router.post("/signUp", userController.signup__post);
 
 router.get("/login", (req, res) => {
   res.send("login page");
