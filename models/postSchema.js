5d276419ebb9c5dff2dd02889df6b4b07ceaa294
Kitsunebackfire <kwolz92@gmail.com>
2022-12-09 11:35:12 -0600

    add dateAndTime virtual for message schema, custom return string for desired format

--- models/postSchema.js
+++ models/postSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const messageSchema = new mongoose.Schema({
   title: {
@@ -18,6 +19,18 @@ const messageSchema = new mongoose.Schema({
     ref: "User",
     required: true,
   },
+});
+
+messageSchema.virtual("dateAndTime").get(function () {
+  //formatted form: 2022-12-08, 17:02
+  const formatted = DateTime.fromJSDate(this.timestamp).toFormat(
+    "yyyy-MM-dd,HH:mm"
+  );
+  const year = formatted.substring(0, 4);
+  const month = formatted.substring(5, 7);
+  const day = formatted.substring(8, 10);
+  const time = formatted.substring(11, 16);
+  return `${day} ${month} ${year}, ${time}`;
 });
 
 module.exports = mongoose.model("Message", messageSchema);
